d378810ebb5d6739e9b7385d8efafbe5736c49ab
unixslayer <piotr.zajac@unixslayer.pl>
2021-05-25 19:13:08 +0200

    - proxying original input parameters

--- src/Command/ReplaceCommand.php
+++ src/Command/ReplaceCommand.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Unixslayer\ComposerReplace\Command;
 
 use Composer\Command\BaseCommand;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -24,18 +26,27 @@ class ReplaceCommand extends BaseCommand
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $inputProxy = $this->prepareInputProxy($input->getArgument('replace'), $input);
         $removeCommand = $this->getApplication()->find('remove');
-
-        $input->setArgument('packages', $input->getArgument('replace'));
-        $ret = $removeCommand->execute($input, $output) ?? 0;
+        $ret = $removeCommand->execute($inputProxy, $output) ?? 0;
 
         if ($ret !== 0) {
             return $ret;
         }
 
+        $inputProxy = $this->prepareInputProxy($input->getArgument('with'), $input);
         $requireCommand = $this->getApplication()->find('require');
-        $input->setArgument('packages', $input->getArgument('with'));
 
-        return $requireCommand->execute($input, $output) ?? 0;
+        return $requireCommand->execute($inputProxy, $output) ?? 0;
+    }
+
+    private function prepareInputProxy(string $package, InputInterface $input): InputInterface
+    {
+        $parameters = ['packages' => $package];
+        foreach (array_keys($input->getOptions()) as $key) {
+            $parameters['--'.$key] = $input->getOption($key);
+        }
+
+        return new ArrayInput($parameters);
     }
 }
